d46c4a1dc6f98fcaa473b1684799310f907969fa
nikkihobman <55224817+nikkihobman@users.noreply.github.com>
2021-08-31 09:10:21 +1000

    Add files via upload
    Pin assignments updated. All trigger pins are connected to GPIO 18.

--- ultrasonicFor4.py
+++ ultrasonicFor4.py
@@ -4,6 +4,7 @@
 
 #Libraries
 import RPi.GPIO as GPIO
+
 import time
  
 #GPIO Mode (BOARD / BCM)
@@ -64,7 +65,7 @@ def distance(GPIO_TRIGGER,GPIO_ECHO):
  
     return distance
 
-def distanceTrigger(distance)
+def distanceTrigger(distance):
     # returns true if the distnce is less than 5
     # false otherwise  
     return distance<=5
@@ -74,38 +75,45 @@ def distanceTrigger(distance)
 if __name__ == '__main__':
     try:
         while True:
+            
+            
             dist_front = distance(GPIO_TRIGGER_FRONT,GPIO_ECHO_FRONT)
             dist_back = distance(GPIO_TRIGGER_BACK,GPIO_ECHO_BACK)
             dist_left = distance(GPIO_TRIGGER_LEFT,GPIO_ECHO_LEFT)
             dist_right = distance(GPIO_TRIGGER_RIGHT,GPIO_ECHO_RIGHT)
             
             
-            # wall defense method
-            if(distanceTrigger(dist_front)): # because our robot will move predominantly in the front direction
-                # step 1 stop motor 
-                robo_stop_flag = 1
-                #code to turn robot in the desiered direction 
-                if(distanceTrigger(dist_right) and !distanceTrigger(dist_left)) # both sides are free to move
-                    # choose a predominat direction or 
-                    # my suggestion: choose the direction with greater distance to the wall 
+            # # wall defense method
+            # if(distanceTrigger(dist_front)): # because our robot will move predominantly in the front direction
+            #     # step 1 stop motor 
+            #     robo_stop_flag = 1
+            #     #code to turn robot in the desiered direction 
+            #     if(distanceTrigger(dist_right) and !distanceTrigger(dist_left)) # both sides are free to move
+            #         # choose a predominat direction or 
+            #         # my suggestion: choose the direction with greater distance to the wall 
                     
                 
-                elif(distanceTrigger(dist_right) and !distanceTrigger(dist_left)): #if right side is blocked 
-                    #turn 90 deg to the left
-                elif(distanceTrigger(dist_left) and !distanceTrigger(dist_right)): # if left side is blocked
-                    #turn 90 deg to the right
-                elif(distanceTrigger(dist_left) and distanceTrigger(dist_right) and !distanceTrigger(dist_back)): #if both sides are blocked  
-                    # turn 180 deg 
-                elif(distanceTrigger(dist_left) and distanceTrigger(dist_right) and distanceTrigger(dist_back)): # all directions blocked
-                    # wait for it to get clear maybe the other robot is close by
-                    time.sleep(5)
-                    while(distanceTrigger(dist_left) and distanceTrigger(dist_right) and distanceTrigger(dist_back))
-                        #rotate 10 deg 
-                        dist_back = distance(GPIO_TRIGGER_BACK,GPIO_ECHO_BACK)
-                        dist_left = distance(GPIO_TRIGGER_LEFT,GPIO_ECHO_LEFT)
-                        dist_right = distance(GPIO_TRIGGER_RIGHT,GPIO_ECHO_RIGHT)
+            #     elif(distanceTrigger(dist_right) and !distanceTrigger(dist_left)): #if right side is blocked 
+            #         #turn 90 deg to the left
+            #     elif(distanceTrigger(dist_left) and !distanceTrigger(dist_right)): # if left side is blocked
+            #         #turn 90 deg to the right
+            #     elif(distanceTrigger(dist_left) and distanceTrigger(dist_right) and !distanceTrigger(dist_back)): #if both sides are blocked  
+            #         # turn 180 deg 
+            #     elif(distanceTrigger(dist_left) and distanceTrigger(dist_right) and distanceTrigger(dist_back)): # all directions blocked
+            #         # wait for it to get clear maybe the other robot is close by
+            #         time.sleep(5)
+            #         while(distanceTrigger(dist_left) and distanceTrigger(dist_right) and distanceTrigger(dist_back))
+            #             #rotate 10 deg 
+            #             dist_back = distance(GPIO_TRIGGER_BACK,GPIO_ECHO_BACK)
+            #             dist_left = distance(GPIO_TRIGGER_LEFT,GPIO_ECHO_LEFT)
+            #             dist_right = distance(GPIO_TRIGGER_RIGHT,GPIO_ECHO_RIGHT)
+            
+            
                     
-            print ("Measured Distance = %.1f cm" % dist)
+            print ("Measured Distance = %.1f cm" % dist_front)
+            print ("Measured Distance = %.1f cm" % dist_back)
+            print ("Measured Distance = %.1f cm" % dist_left)
+            print ("Measured Distance = %.1f cm" % dist_right)
             time.sleep(1)
  
         # Reset by pressing CTRL + C
